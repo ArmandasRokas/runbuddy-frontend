ece235a4df9b75d5d594ac7d335fe343cb71bdcf
Armandas <armandas.rokas@gmail.com>
2019-06-02 14:04:13 +0200

    added error component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  errorMessage = 'Invalid Credentials'
+  username = 'username'
+  password = ''
+  invalidLogin = false
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  handleLogin() {
+    if (this.username === "username" && this.password === "pass") {
+      this.invalidLogin = false;
+    } else {
+      this.invalidLogin = true;
+    }
+  }
 }
