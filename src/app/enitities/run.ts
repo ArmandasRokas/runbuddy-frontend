0cc410675875643cc5b21c7278ebe9fb49ab56b2
FilipBLJensen <filblj@gmail.com>
2019-06-11 13:44:01 +0200

    Added run entity

--- src/app/enitities/run.ts
+++ src/app/enitities/run.ts
@@ -1,7 +1,16 @@
 import { WayPoint } from './route';
+import { Route } from './route';
 
 export class Checkpoint {
   constructor(
     public wayPoint: WayPoint,
     public visitedTimestamp: number) { }
+}
+
+export class Run {
+  private id: String;
+    private route: Route;
+    private checkpoints: Checkpoint[];
+    private startTime: Date;
+    private endTime: Date;
 }
