fdad380426bf5427f799e84711c607a83a4f295e
Armandas <armandas.rokas@gmail.com>
2019-06-19 15:43:10 +0200

    added animated text

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { ViewChild, AfterViewInit } from '@angular/core';
+import { TextAnimation, Teximate } from 'ngx-teximate';
+import { fadeIn } from 'ng-animate';
 
 @Component({
   selector: 'app-about-us',
   templateUrl: './about-us.component.html',
   styleUrls: ['./about-us.component.css']
 })
-export class AboutUsComponent implements OnInit {
 
-  constructor() { }
+export class AboutUsComponent {
 
-  ngOnInit() {
-  }
+  name = 'Angular';
+  text = ' We are group of three persons from Danish Technical University. This project is made for the course Advanced programming. We hope that you will enjoy using this application that will help you to arrange the most incredible races you could ever imagined.';
+
+  enterAnimation: TextAnimation = {
+    animation: fadeIn,
+    delay: 50,
+    type: 'letter'
+  };
 
 }
